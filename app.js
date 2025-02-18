1a58591e51396fa3077601c38f658ea4b80bf151
Juanchokajcrr <juanchokajcrr@gmail.com>
2025-02-18 01:23:55 -0500

    Juego sorteo amigo secreto funcional

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo(amigo) {
+    let amigoAgregado = document.getElementById('amigo').value;
+    if (amigoAgregado === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        listaAmigos.push(amigoAgregado);
+        console.log(listaAmigos);
+        actualizarListaAmigos();
+        limpiarCaja();
+        return;
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+    return;
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    // Limpiar la lista existente
+    lista.innerHTML = ''; 
+    for (let i = 0; i < listaAmigos.length; i++) {
+        const elemento = document.createElement("li");
+        elemento.textContent = listaAmigos[i];
+        lista.appendChild(elemento);
+    }
+}
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert('Por favor, agregue al menos un amigo.');
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        let amigoSorteado = listaAmigos[indiceAleatorio];
+        asignarTextoElemento('#resultado', `El amigo secreto sorteado es: ${amigoSorteado}`);
+    }
+}
